9411a835b9c2075df0b086b764f0b95862674578
tbauers <thomas.bauers@engagedtechnologies.com>
2017-05-22 08:15:09 -0500

    staging up a wip of a service to grab zillow data

---
+++ ZillowService/Controllers/ZillowController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Linq;
+using System.Xml.XPath;
+using System.Xml.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZillowService.Controllers
+{
+    [Route("api/[controller]")]
+    public class ZillowController : Controller
+    {
+        private HttpClient httpClient;
+
+        XPathDocument zillowResult;
+
+        public ZillowController()
+        {
+            this.httpClient = new HttpClient();
+            this.httpClient.BaseAddress = new Uri("http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz197buben8jv_2ghte&state=mo&city=kansascity&childtype=neighborhood");
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            using (httpClient)
+            {
+                try
+                {
+                    var response =
+                      await this.httpClient.GetAsync(new Uri("http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz197buben8jv_2ghte&state=mo&city=kansascity&childtype=neighborhood"));
+                    
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    
+                    XmlSerializer serializer = new XmlSerializer(typeof(RootObject));
+                    using (TextReader reader = new StringReader(responseString))
+                    {
+                      zillowResult = new XPathDocument(reader);
+                    }
+
+                    var nav = zillowResult.CreateNavigator();
+                    var result = nav.Evaluate("RegionChildren");
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+                
+            }
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ZillowService/ZillowResponse.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZillowService
+{
+  public class Message
+  {
+    public string code { get; set; }
+    public string text { get; set; }
+  }
+
+  public class Request
+  {
+    public string childtype { get; set; }
+    public string city { get; set; }
+    public string state { get; set; }
+  }
+
+  public class Region
+  {
+    public string id { get; set; }
+    public string latitude { get; set; }
+    public string longitude { get; set; }
+    public string name { get; set; }
+    public string url { get; set; }
+  }
+
+  public class List
+  {
+    public string count { get; set; }
+    public List<Region> region { get; set; }
+  }
+
+  public class Region2
+  {
+    public string id { get; set; }
+    public string latitude { get; set; }
+    public string longitude { get; set; }
+  }
+
+  public class Response
+  {
+    public List list { get; set; }
+    public Region2 region { get; set; }
+    public string subregiontype { get; set; }
+  }
+
+  public class RegionChildrenRegionchildren
+  { 
+    public string regionChildren { get; set; }
+    public string xsi { get; set; }
+    public string schemaLocation { get; set; }
+    public Message message { get; set; }
+    public Request request { get; set; }
+    public Response response { get; set; }
+  }
+
+  public class RootObject
+  {
+    public RegionChildrenRegionchildren regionChildren { get; set; }
+  }
+}
