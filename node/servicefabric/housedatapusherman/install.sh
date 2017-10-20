f7f1afefd71319f70a6e975d175838aa3891fd2d
Thomas Bauers <thomas.bauers@engagedtechnologies.com>
2017-10-20 17:00:05 -0500

    adding some kube deploy stuff removing my azure service fabric deploy stuff

--- node/servicefabric/housedatapusherman/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sfctl application upload --path housedatapusherman --show-progress
-sfctl application provision --application-type-build-path housedatapusherman
-sfctl application create --app-name fabric:/housedatapusherman --app-type housedatapushermanType --app-version 1.0.0
--- node/servicefabric/housedatapusherman/uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sfctl application delete --application-id housedatapusherman
-sfctl application unprovision --application-type-name housedatapushermanType --application-type-version 1.0.0
-sfctl store delete --content-path housedatapusherman
